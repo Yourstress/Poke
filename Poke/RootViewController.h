71a87e18248fe19080f1d81979f82af9c3a8b823
Mansour Al-Sarraf <yourstress@gmail.com>
2011-11-28 04:32:20 +0300

    Initial Commit

---
+++ Poke/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Poke
+//
+//  Created by Macbook Pro on 11/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
